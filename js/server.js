ed31e0257b8dc137be9a75acb791532e2bee4079
Byron.Samaripa@gmail.com <Byron.Samaripa@colorado.edu>
2014-06-25 17:59:45 -0600

    updating stuff

--- js/server.js
+++ js/server.js
@@ -9,12 +9,8 @@ var mongo = require('mongoose');
 var dbName = "/mytest";
 var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + dbName;
 
-mongo.connect(connection_string);
+//mongo.connect(connection_string);
+app.
 
-
-
-app.get('*', function(req, res) {
-	res.sendfile('../index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
-
-app.listen(3000);
+app.listen(8000);
+console.log("listening on port 8000");
