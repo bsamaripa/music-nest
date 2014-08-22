2956788125c8876725e346a4dd418ffa29e6ba09
Byron Samaripa <Byron.Samaripa@colorado.edu>
2014-08-22 14:08:03 -0600

    Added jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,14 @@
+//  Not ready! Do not use!
 module.exports = function(grunt) {
-  grunt.registerTask('default', []);
+	grunt.initConfig({
+	  jshint: {
+	    all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+	  },
+	});
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-mocha');
+
+	grunt.registerTask('default', ['jshint']);
 
 };
